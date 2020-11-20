d572253f3105e17380e9b71a63fb9e72942f9a01
becode <robinmcgregor@gmail.com>
2020-11-20 08:26:09 +0100

    deleted app folder

--- app/app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import io
-import cv2
-
-st.set_option('deprecation.showfileUploaderEncoding', False)
-
-st.title("Facial emotion analytics")
-
-uploaded_file = st.file_uploader("Choose a video...", type=["mp4"])
